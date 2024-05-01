c232d516d5603968833c12a15d1c0cc7994620c8
Naoufal Jrhaider <naougaljrh2003@gmail.com>
2024-05-01 17:46:52 +0200

    Loding data animation in profile card

--- src/Components/ui/CarteProfile.jsx
+++ src/Components/ui/CarteProfile.jsx
@@ -10,14 +10,19 @@ import { useEffect, useState } from "react";
 function CarteProfile() {
   const [user, setUser] = useState({});
   const [signingOut, setSigningOut] = useState(false);
+  const [loadingData, setLoadingData] = useState(false);
   const router = useRouter();
   useEffect(() => {
     try {
+      setLoadingData(true);
       getDataFromToken().then((rs) => setUser(rs));
     } catch (error) {
       throw new Error(error.message);
+    } finally {
+      setLoadingData(false);
     }
   }, []);
+  // useEffect(() => console.log(user), [user]);
   const handleLogout = async () => {
     try {
       setSigningOut(true);
@@ -31,34 +36,40 @@ function CarteProfile() {
   };
   return (
     <div className="flex flex-col justify-center items-center gap-2 text-center">
-      <h3 className="text-sm font-meduim text-gray-900">
-        Naoufaljrh2000@gmail.com
-      </h3>
-      <Avatar className="h-[80px] w-[80px]">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>TM</AvatarFallback>
-      </Avatar>
-      <h1 className="text-lg font-bold">Jrhaider Naoufal</h1>
-      <h2 className="text-sm font-medium text-gray-500">
-        Cybersecurity & Intelligence Artificielle
-      </h2>
-      <span className="text-xs text-muted-foreground">
-        Première année, Semester 2
-      </span>
+      {loadingData ? (
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        <>
+          <h3 className="text-sm font-meduim text-gray-900">{user.email}</h3>
+          <Avatar className="h-[80px] w-[80px]">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>TM</AvatarFallback>
+          </Avatar>
+          <h1 className="text-lg font-bold">
+            {user.firstname} {user.lastname}
+          </h1>
+          <h2 className="text-sm font-medium text-gray-500">
+            Cybersecurity & Intelligence Artificielle
+          </h2>
+          <span className="text-xs text-muted-foreground">
+            Première année, Semester 2
+          </span>
 
-      <Button
-        variant="ghost"
-        className="w-full border-t mt-2"
-        onClick={handleLogout}
-        disabled={signingOut}
-      >
-        {signingOut ? (
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <LogOutIcon />
-        )}
-        Déconnecté
-      </Button>
+          <Button
+            variant="ghost"
+            className="w-full border-t mt-2"
+            onClick={handleLogout}
+            disabled={signingOut}
+          >
+            {signingOut ? (
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              <LogOutIcon />
+            )}
+            Déconnecté
+          </Button>
+        </>
+      )}
     </div>
   );
 }
